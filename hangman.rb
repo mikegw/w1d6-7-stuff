e236cb174425776a75267bfbf45c17ee4144fbad
mike <mikewilliamson86@gmail.com>
2014-09-14 14:34:43 -0400

    Full game class

--- hangman.rb
+++ hangman.rb
@@ -5,7 +5,7 @@ class Hangman
    |  |
    | _O_
    |  |
-   | / \
+   | / \\
 ___|____","
     __
    |  |
@@ -16,7 +16,7 @@ ___|____","
     __
    |  |
    | _O_
-   |  |
+   |
    |
 ___|____","
     __
@@ -42,19 +42,21 @@ ___|____","
 
 
 
-___|____"]
+___|____"].reverse
 
   attr_reader :guesser, :word_chooser, :dictionary
 
-  def initialize(guesser, word_chooser)
+  def initialize(guesser = nil, word_chooser = nil)
     @public_word = nil
     @guesser = guesser
     @word_chooser = word_chooser
     @dictionary
+    @wrong_guesses_left = PICS.length - 1
+    #play
   end
 
   def play
-    game_start_disp
+    game_start_display
     ask_for_word
     loop do
       display_public_word
@@ -69,8 +71,40 @@ ___|____"]
     end
   end
 
+  def game_start_display
+    puts "\n\t\t\tHANGMAN!!
+    #{PICS.map{|pic| pic.split("\n")}.transpose.map{|line_arr| line_arr.map{|line| '%-9.9s' % line}.join}.join("\n")}"
+    #puts PICS.map{|pic| pic.split("\n")}
+  end
+
+
   def ask_for_word
     self.public_word = self.word_chooser.pick_word
   end
+
+  def display_public_word
+    puts "Secret word is: " + self.public_word
+  end
+
+  def game_over?
+    secret_word.include?("_") || wrong_guesses_left == 0
+  end
+
+  def game_end_display
+    if secret_word.include?("_")
+      puts "Congratulations, #{self.guesser.name}, you won!"
+    else
+      puts "Hard luck, #{self.guesser.name} wins this time..."
+    end
+  end
+
+
+  def ask_new_game
+    puts "Would you like to play again? (y/n)"
+    play_again? = gets
+    Hangman.new(guesser, word_chooser) if play_again == y
+  end
+
+
 
 end

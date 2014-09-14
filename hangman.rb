9399dde929060c88cee5b2059d576b384f8c573c
mike <mikewilliamson86@gmail.com>
2014-09-14 13:31:07 -0400

    "play loop"

--- hangman.rb
+++ hangman.rb
@@ -1,9 +1,76 @@
 class Hangman
 
-  attr_accessor
-  def initialize
-    @secret_word = nil
+  PICS = ["
+    __
+   |  |
+   | _O_
+   |  |
+   | / \
+___|____","
+    __
+   |  |
+   | _O_
+   |  |
+   |
+___|____","
+    __
+   |  |
+   | _O_
+   |  |
+   |
+___|____","
+    __
+   |  |
+   |  O
+   |
+   |
+___|____","
+    __
+   |  |
+   |
+   |
+   |
+___|____","
+
+   |
+   |
+   |
+   |
+___|____","
+
+
+
+
+
+___|____"]
+
+  attr_reader :guesser, :word_chooser, :dictionary
+
+  def initialize(guesser, word_chooser)
+    @public_word = nil
+    @guesser = guesser
+    @word_chooser = word_chooser
+    @dictionary
+  end
+
+  def play
+    game_start_disp
+    ask_for_word
+    loop do
+      display_public_word
+      guess = guesser.take_a_guess
+
+      word_chooser.handle(guess)
+      if game_over?
+        ask_new_game
+        break
+      end
+
+    end
+  end
+
+  def ask_for_word
+    self.public_word = self.word_chooser.pick_word
   end
 
 end
-

d3e90e0835a3ddb35f9e625c19d6baaac74c06d2
mike <mikewilliamson86@gmail.com>
2014-09-14 17:09:02 -0400

    Player Class before debugging

--- hangman.rb
+++ hangman.rb
@@ -42,9 +42,10 @@ ___|____","
 
 
 
-___|____"].reverse
+___|____"]
 
-  attr_reader :guesser, :word_chooser, :dictionary
+  attr_accessor :guesser, :word_chooser, :dictionary,
+                :public_word, :wrong_guesses_left
 
   def initialize(guesser = nil, word_chooser = nil)
     @public_word = nil
@@ -52,7 +53,7 @@ ___|____"].reverse
     @word_chooser = word_chooser
     @dictionary
     @wrong_guesses_left = PICS.length - 1
-    #play
+    play
   end
 
   def play
@@ -62,10 +63,13 @@ ___|____"].reverse
       display_public_word
       guess = guesser.take_a_guess
       case word_chooser.handle(guess)
-        wrong_guesses_left = 0 when :wrong_word
-        wrong_guesses_left -= 1 when :wrong_letter
+        when :wrong_word
+          self.wrong_guesses_left = 0
+        when :wrong_letter
+          self.wrong_guesses_left -= 1
       end
       if game_over?
+        game_end_display
         ask_new_game
         break
       end
@@ -75,8 +79,8 @@ ___|____"].reverse
 
   def game_start_display
     puts "\n\t\t\tHANGMAN!!
-    #{PICS.map{|pic| pic.split("\n")}.transpose.map{|line_arr| line_arr.map{|line| '%-9.9s' % line}.join}.join("\n")}"
-    #puts PICS.map{|pic| pic.split("\n")}
+    #{PICS.reverse.map{|pic| pic.split("\n")}.transpose.map{|line_arr| line_arr.map{|line| '%-9.9s' % line}.join}.join("\n")}"
+    puts "\n"
   end
 
 
@@ -85,26 +89,28 @@ ___|____"].reverse
   end
 
   def display_public_word
+    puts PICS[self.wrong_guesses_left]
     puts "Secret word is: " + self.public_word
   end
 
   def game_over?
-    secret_word.include?("_") || wrong_guesses_left == 0
+    !self.word_chooser.public_word.include?("_") || wrong_guesses_left == 0
   end
 
   def game_end_display
-    if secret_word.include?("_")
-      puts "Congratulations, #{self.guesser.name}, you won!"
+    if public_word.include?("_")
+      puts PICS.first
+      puts "\nHard luck, #{self.guesser.name}, #{self.word_chooser.name} wins this time..."
     else
-      puts "Hard luck, #{self.guesser.name} wins this time..."
+      puts "\nCongratulations, #{self.guesser.name}, you won!"
     end
   end
 
 
   def ask_new_game
-    puts "Would you like to play again? (y/n)"
+    puts "\nWould you like to play again? (y/n)"
     play_again = gets.chomp.downcase
-    Hangman.new(guesser, word_chooser) if play_again == y
+    Hangman.new(guesser, word_chooser) if play_again == "y"
   end
 
 
@@ -113,34 +119,36 @@ end
 
 class HumanPlayer
 
+  attr_accessor :public_word, :player_type, :name, :confused_speak
+
   def initialize(player_type)
     @public_word = nil
-    ask_for_name
-    @player_type
+    @player_type = player_type
+    ask_for_name(player_type)
   end
 
-  def ask_for_name
+  def ask_for_name(player_type)
     print "#{self.player_type}'s name:  "
     @name = gets.chomp
   end
 
   def take_a_guess
-    "{self.name.capitalize}, take a guess: #{gets.chomp}"
+    print "#{self.name.capitalize}, take a guess: "
+    guess = gets.chomp
   end
 
   def handle(guess)
     if guess.length > 1
-      print "{self.name.capitalize}, is #{guess} your word?(y/n): "
-      gets.chomp.downcase == "y" ? public_word = guess : return :wrong_word
+      print "#{self.name.capitalize}, is #{guess} your word?(y/n): "
+      gets.chomp.downcase == "y" ? public_word = guess : (return :wrong_word)
     end
 
-    print "{self.name.capitalize}, is #{guess} in your word? (y/n): "
+    print "#{self.name.capitalize}, is #{guess} in your word? (y/n): "
     if gets.chomp.downcase == "y"
       print "where?: "
       positions = gets.chomp.downcase.split(" ")
-
       positions.each do |i|
-        self.public_word[i] = guess
+        (self.public_word)[i.to_i] = guess
       end
 
     else
@@ -161,6 +169,10 @@ class ComputerPlayer
   def initialize
     @secret_word = nil
     ask_for_name
+    @confused_speak = ["Sorry, that makes no sense to me, but then I'm just a computer...",
+                       "What the... I don't...",
+                       "You really don't understand this game do you?"
+                       "I'm going to go to bed now.  Why don't you go read a rule book?"]
   end
 
   def ask_for_name
@@ -168,15 +180,35 @@ class ComputerPlayer
     @name = gets
     puts @name
   end
-  def take_a_guess
-    puts ""
-    gets
+
+  def confused
+    confused_speak.empty? ? :wrong_word : confused[0].shift
   end
 
   def handle(guess)
+    if guess.length == public_word.length
+      secret_word == guess ? public_word = guess : (return :wrong_word)
+    end
+
+    if guess.length.between(2,guess.length)
+      return confused
+    end
+
+    if @secret_word.include?(guess)
+      public_word.each_char.with_index.map do |l,i|
+        guess == secret_word[i] ? l = guess : "_"
+      end
+    end
+
+    else
+      return :wrong_letter
+    end
+
+    :handled
   end
 
   def pick_word
-
+    self.secret_word = self.dictionary.sample
+    self.public_word = '_' * self.secret_word.length
   end
 end

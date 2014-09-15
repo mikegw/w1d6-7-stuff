02070ecd37763b736f1fcc01e13fbaf05a22233a
mike <mikewilliamson86@gmail.com>
2014-09-14 21:16:17 -0400

    Phase 2a

--- wordchains.rb
+++ wordchains.rb
@@ -1,22 +1,50 @@
 class Wordchainer
+
+  attr_accessor :dictionary, :all_seen_words, :current_words
+
   def initialize(dictionary_file)
     @dictionary = File.readlines(dictionary_file).map(&:chomp)
+    @all_seen_words = []
+    @current_words = []
   end
 
-  def adjacent_words(word)
-    dict = @dictionary.keep_if{|w| w.length == word.length }
+  def adjacent_words(word, dict = nil)
     adj_words = []
+    dict = @dictionary.keep_if{|w| w.length == word.length } if dict.nil?
     ('a'..'z').each do |l|
-      (word.length-1).times do |i|
+      (word.length).times do |i|
         next if l == word[i]
         new_word = word.dup
         new_word[i] = l
         adj_words << new_word if dict.include?(new_word)
       end
     end
+
     adj_words
   end
+
+  def run(source, target)
+    dict = @dictionary.keep_if{|w| w.length == source.length }
+    self.all_seen_words << source
+    self.current_words << source
+    until self.current_words.empty?
+      new_current_words = []
+      @current_words.each do|word|
+        adjacent_words(word, dict).each do |new_word|
+          next if self.all_seen_words.include?(new_word)
+          new_current_words << new_word
+          self.all_seen_words << new_word
+        end
+      end
+      p self.current_words
+      self.current_words = new_current_words
+    end
+    self.all_seen_words = []
+  end
+
 end
 
 w = Wordchainer.new('dictionary.txt')
-p w.adjacent_words('angle')
+#p w.adjacent_words('angle')
+
+w.run('cat', 'bat')
